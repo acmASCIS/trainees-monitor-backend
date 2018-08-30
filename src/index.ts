c9a82b96b794a6f134242cc3e8cad29d521fc23e
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-08-30 20:56:11 +0200

    Added mongoose and connecting to the database

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import Server from './Server';
 
+dotenv.load();
+
 const server: Server = new Server();
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/trainees-monitor';
+mongoose.connect(
+  MONGO_URI,
+  { useNewUrlParser: true },
+  error => {
+    if (error) {
+      console.log('Failed to connect to MongoDB');
+    }
+    console.log('Connected successfuly to MongoDB');
+  }
+);
+
 export default server;
