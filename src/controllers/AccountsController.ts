8083aa4be020749ca193b26275d9d2f0ef70faa1
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-08-31 20:10:01 +0200

    Implemented POST /register endpoint

--- src/controllers/AccountsController.ts
+++ src/controllers/AccountsController.ts
@@ -1,12 +1,41 @@
-import { Application, Request, Response } from 'express';
+import { ApiError } from './../utils/ApiError';
+import { Application, Request, Response, NextFunction } from 'express';
+import _ from 'lodash';
+
+import { IUserRepository, UserRepository } from './../repositories/UserRepository';
+import { UserDTO } from './../models/User/UserDTO';
 import { IController } from './IController';
+import { validateRegisterInput } from '../utils/validator';
+import { User } from '../models/User/User';
 
 export default class AccountsController implements IController {
+  private userRepository: IUserRepository = new UserRepository();
+
   public register(app: Application): void {
-    app.get('/', this.ping);
+    app.post('/register', this.registerUser.bind(this));
   }
 
-  private ping(req: Request, res: Response) {
-    res.json('Hello World!');
+  private async registerUser(req: Request, res: Response, next: NextFunction) {
+    const { errors } = validateRegisterInput(req.body);
+    if (errors) {
+      next(new ApiError('Invalid Input', 400, errors));
+      return;
+    }
+
+    const { handle, name, email, password, role, codeforcesHandle } = req.body;
+    const user: User = new User(handle, name, email, password, role, {
+      codeforces: codeforcesHandle
+    });
+    user.password = await user.hashPassword();
+
+    // Checking duplicate user email and handle
+    if (await this.userRepository.findOne({ email: user.email })) {
+      next(new ApiError('Invalid Input', 400, { email: 'Email already exists' }));
+    } else if (await this.userRepository.findOne({ handle: user.handle })) {
+      next(new ApiError('Invalid Input', 400, { handle: 'Handle already exists' }));
+    } else {
+      const createdUser: UserDTO = await this.userRepository.create(user);
+      res.status(201).json(_.pick(createdUser, ['_id', 'handle', 'name']));
+    }
   }
 }
