b320dd32176cd528fb95dbe029a4805125952a32
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-10-15 02:23:00 +0200

    Fixed env loading bug
    - production env file typo in envConfig
    - loading dev env file in tests

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 // Loading ENV file
 switch (process.env.ENV) {
   case 'production':
-    dotenv.config({ path: 'env.prod' });
+    dotenv.config({ path: '.env.prod' });
     break;
   case 'staging':
     dotenv.config({ path: '.env.staging' });
