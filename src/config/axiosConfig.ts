353aa8323cc58f7dc3813266a31ab2416a1c3832
Khaled Mohamed <khal3d.mohamed@gmail.com>
2018-10-24 09:33:55 +0200

    Codeforces Too many requests bug fix
    - Changed the contest analysis to a complete synchronous code to avoid multiple requests at the same time
    - changed retries count to 3

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -3,7 +3,7 @@ import axiosRetry from 'axios-retry';
 
 // Setting up retry in case of 429 response (Too many requests)
 axiosRetry(axios, {
-  retries: 10,
+  retries: 3,
   retryCondition: error => {
     return error.response ? error.response.status === 429 : false;
   },
