ceafc7c2d757d6bac41fc516550bf13cc0d8f261
AhmedRagab <aragab22@gmail.com>
2021-01-15 17:42:52 +0200

    fix:fix packagelock types version

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,17 @@ import Server from './Server';
 const server: Server = new Server();
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/trainees-monitor';
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (error) => {
-  if (error) {
-    console.log('Failed to connect to MongoDB');
-    return;
-  }
+mongoose.connect(
+  MONGO_URI,
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+  (error) => {
+    if (error) {
+      console.log('Failed to connect to MongoDB');
+      return;
+    }
 
-  console.log('Connected successfuly to MongoDB');
-});
+    console.log('Connected successfuly to MongoDB');
+  }
+);
 
 export const app = server.start();
